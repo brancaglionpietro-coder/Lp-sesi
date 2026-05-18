427e3cc23195e4e87380f1920ed91e9f3f3a9177
brancaglionpietro-coder <brancaglionpietro@gmail.com>
2026-05-18 08:40:33 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -37,4 +37,15 @@ const observer = new IntersectionObserver((entries) => {
     })
 }, {threshold: 0.2})
 
-observer.observe(document.querySelector('#s2'));
+observer.observe(document.querySelector('#s2'))
+
+function enviar(){
+    const nome = document.getElementById('nome').value
+    const assunto = document.getElementById('assunto').value
+
+    //montar a string (mensagem)
+    const mensagem = `Gostaria de entrar em contato\n\nNome: ${nome}\nAssunto: ${assunto}`
+    //interpretar caracteres especiais e espaços entre palavras
+    const msg = encodeURIComponent(mensagem)
+    window.open(`https://wa.me/5541991434966?text=${msg}`)
+}
